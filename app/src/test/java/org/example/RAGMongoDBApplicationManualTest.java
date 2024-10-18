a618f1fe772f16b99c52d9bef3e1f480bc5f0e13
jrgleason <jackiegleason@gmail.com>
2024-10-18 19:52:50 -0400

    Forgot to import

--- app/src/test/java/org/example/RAGMongoDBApplicationManualTest.java
+++ app/src/test/java/org/example/RAGMongoDBApplicationManualTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
